0d14b79cf37f15d22e1b5b648fa37890cfa3f0fc
Martin Larsson <martin.larsson.post@gmail.com>
2017-03-28 16:14:39 +0200

    autent new try

--- authent.js
+++ authent.js
@@ -7,7 +7,11 @@ var provider = new firebase.auth.GithubAuthProvider();
 btnLogin.addEventListener('click', function (ev) {
     console.log('pressed');
     firebase.auth().signInWithPopup(provider).then(function (result) {
-        let user = result.user;
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
     }).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
@@ -16,7 +20,6 @@ btnLogin.addEventListener('click', function (ev) {
         var email = error.email;
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
-        console.log(errorCode);
-        console.log(errorMessage);
+        // ...
     });
 });
