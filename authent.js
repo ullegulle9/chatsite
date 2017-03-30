fc5dc38e5eb260aadb185a30b8c1f57c2cda2f75
Martin Larsson <martin.larsson.post@gmail.com>
2017-03-30 09:53:32 +0200

    window onload

--- authent.js
+++ authent.js
@@ -1,31 +1,31 @@
-let btnLogin = document.getElementById('loginBtn');
-let wlcDiv = document.getElementById('msgDiv');
+window.onload = function () {
+    let btnLogin = document.getElementById('loginBtn');
+    let wlcDiv = document.getElementById('msgDiv');
 
-var provider = new firebase.auth.GithubAuthProvider();
+    var provider = new firebase.auth.GithubAuthProvider();
+    btnLogin.addEventListener('click', function (ev) {
+        console.log('pressed');
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log(token);
+            console.log(user);
+            wlcDiv.innerHTML = `Welcome ${user}`;
 
-
-btnLogin.addEventListener('click', function (ev) {
-    console.log('pressed');
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        console.log(token);
-        console.log(user);
-        wlcDiv.innerHTML = `Welcome ${user}`;
-        
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        console.log(errorCode);
-        console.log(errorMessage);
-        console.log(email);
-        console.log(credential);
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log(errorCode);
+            console.log(errorMessage);
+            console.log(email);
+            console.log(credential);
+        });
     });
-});
+}
